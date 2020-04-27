bcf5186a86835d01cca7bac31a8fa6f203a9ffeb
Nikita Stroganov <Findoss@yandex.ru>
2020-04-27 13:08:52 +0300

    fix typo

--- src/screens/App/App.tsx
+++ src/screens/App/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import Chat from '../../components/Chat/Chat';
+// import Chat from '../../components/Chat/Chat';
 
 function App() {
-  return (
-    <div className='App'>
-      <Chat />
-    </div>
-  );
+  return <div className='App'>{/* <Chat /> */}</div>;
 }
 
 export default App;

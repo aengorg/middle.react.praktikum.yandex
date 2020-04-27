adcf99c91d9d1ea682cdafad1e556e47983471ab
Nikita Stroganov <Findoss@yandex.ru>
2020-04-27 13:03:34 +0300

    move App to screens

--- src/screens/App/App.tsx
+++ src/screens/App/App.tsx
@@ -1,15 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import ChannelsList from "../../components/Channel/List";
+import Chat from '../../components/Chat/Chat';
 
 function App() {
   return (
-    <div className="App">
-      <div className="sidebar">
-        <ChannelsList />
-      </div>
-      <div className="content">content</div>
+    <div className='App'>
+      <Chat />
     </div>
   );
 }

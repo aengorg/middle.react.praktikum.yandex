ad559d6d28cfcbe182ca5e13d5dcf6702db34094
Nikita Stroganov <Findoss@yandex.ru>
2020-04-28 22:52:22 +0300

    fix types and return

--- src/api/channels.ts
+++ src/api/channels.ts
@@ -1,9 +1,11 @@
 // import { API_URL } from '../constants';
 // import { http, IHttpResponse } from '../utils/http';
 
+import { TChannelsList } from '../components/ChannelList/types';
+
 import testChannels from './static/channels.json';
 
-export const getChannels = async () => {
+export const getChannels = async (): Promise<TChannelsList> => {
   // let data: IHttpResponse<any>;
   try {
     // data = await http<any>(
@@ -13,6 +15,7 @@ export const getChannels = async () => {
     return testChannels;
   } catch (error) {
     console.error(error);
+    return [];
   }
 };
 

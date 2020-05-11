6e7c01c1be9c6d61a032b9b51b5d99d41f8add50
Nikita Stroganov <findoss@yandex.ru>
2020-05-11 19:46:58 +0300

    refactor MessageList to class component

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,41 +1,40 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import './MessageList.scss';
 
 import { genKey } from '../../utils/generation';
 import { Message } from '../Message/Message';
 import { DateSeparator } from '../Message/DateSeparator';
 
-import { IMessage, TUserId } from '../../types';
-import { Props } from './types';
+import { IMessage } from '../../types';
+import { Props, State } from './types';
 
-let lastDay: number = 0;
-const renderDate = (date: number) => {
-  let day: number = new Date(date).getDate();
-  if (day !== lastDay) {
-    lastDay = day;
-    return <DateSeparator key={genKey()} date={date} />;
+export class MessageList extends Component<Props, State> {
+  private renderDate = (currentMsg: IMessage, lastMsg: IMessage | undefined) => {
+    if (lastMsg) {
+      const dayCurrentMsg: number = new Date(currentMsg.message.date).getDate();
+      const dayLastMsg: number = new Date(lastMsg.message.date).getDate();
+      if (dayCurrentMsg === dayLastMsg) return null;
+    }
+    return <DateSeparator date={currentMsg.message.date} />;
+  };
+
+  private renderMessage = (currentMsg: IMessage, lastMsg: IMessage | undefined) => {
+    const isSameUser: boolean = lastMsg !== undefined && currentMsg.user.id === lastMsg.user.id;
+    return <Message {...currentMsg} short={isSameUser} />;
+  };
+
+  public render() {
+    const {messagesList, className} = this.props;
+
+    return (
+      <div className={`${className} message-list`}>
+        {messagesList.map((msg: IMessage, i: number) => (
+          <Fragment key={genKey()}>
+            {this.renderDate(msg, messagesList[i - 1])}
+            {this.renderMessage(msg, messagesList[i - 1])}
+          </Fragment>
+        ))}
+      </div>
+    );
   }
-};
-
-let lastUserId: TUserId = '';
-const renderMessage = (msg: IMessage) => {
-  const isSameUser: boolean = msg.user.id === lastUserId;
-  if (!isSameUser) lastUserId = msg.user.id;
-  return <Message {...msg} short={isSameUser} />;
-};
-
-export const MessageList = ({ className, messagesList }: Props) => {
-  lastDay = 0;
-  lastUserId = '';
-
-  return (
-    <div className={`${className} message-list`}>
-      {messagesList.map((msg: IMessage) => (
-        <React.Fragment key={genKey()}>
-          {renderDate(msg.message.date)}
-          {renderMessage(msg)}
-        </React.Fragment>
-      ))}
-    </div>
-  );
-};
+}

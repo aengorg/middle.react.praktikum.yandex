662fb26384970ba8b897c567cd792fdbf969199a
Nikita Stroganov <findoss@yandex.ru>
2020-05-14 22:26:02 +0300

    add param userId

--- src/api/channels.ts
+++ src/api/channels.ts
@@ -5,11 +5,11 @@
 // import { API_URL } from '../constants';
 // import { http, IHttpResponse } from '../utils/http';
 
-import { TChannelsList } from '../types';
+import { TChannelsList, TUserId } from '../types';
 
 import testChannels from './static/channels.json';
 
-export const getChannels = async (): Promise<TChannelsList> => {
+export const getChannels = async (userId: TUserId): Promise<TChannelsList> => {
   // let data: IHttpResponse<any>;
   try {
     // data = await http<any>(
@@ -24,5 +24,5 @@ export const getChannels = async (): Promise<TChannelsList> => {
 };
 
 export default {
-  getChannels
+  getChannels,
 };

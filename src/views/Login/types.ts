e4dad4de0067ca88ba931a8312aecf3ec4e66775
Nikita Stroganov <findoss@yandex.ru>
2020-05-14 22:33:25 +0300

    add login view

--- src/views/Login/types.ts
+++ src/views/Login/types.ts
@@ -1,15 +1,19 @@
 import { RouteComponentProps } from 'react-router-dom';
 import { LocalizationProp } from '../../containers/localization';
+import { IUser } from '../../types';
 
 interface RouteProps {}
 
-interface OwnProps {}
+interface OwnProps {
+  loginUser(user: IUser): void;
+}
 
 export type Props = OwnProps &
   LocalizationProp &
   RouteComponentProps<RouteProps>;
 
 export type State = {
+  errors: string[];
   username: string;
   password: string;
 };

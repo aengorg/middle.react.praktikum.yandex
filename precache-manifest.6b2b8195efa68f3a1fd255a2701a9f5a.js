ba4a0be3abe03920f1921547b5d24a3597b9acc1
Nikita Stroganov <findoss@yandex.ru>
2020-05-15 17:41:00 +0300

    update

---
+++ precache-manifest.6b2b8195efa68f3a1fd255a2701a9f5a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00667af97a099d1183a4eea9ac37854",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e294f376d1eae655dd96",
+    "url": "./static/css/main.f56bad78.chunk.css"
+  },
+  {
+    "revision": "35d743d1281d75b503ce",
+    "url": "./static/js/2.01b72839.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.01b72839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e294f376d1eae655dd96",
+    "url": "./static/js/main.e85c4fa2.chunk.js"
+  },
+  {
+    "revision": "82ec2a9660ccc635d6e4",
+    "url": "./static/js/runtime-main.7e58cb6f.js"
+  },
+  {
+    "revision": "3084589239646354820fe2e9a710f442",
+    "url": "./static/media/account.30845892.svg"
+  },
+  {
+    "revision": "cd89643f81b909c4463d926bae820ea6",
+    "url": "./static/media/add.cd89643f.svg"
+  },
+  {
+    "revision": "ef93d4d7a9422c5ad903e9cf7e372c67",
+    "url": "./static/media/chat.ef93d4d7.svg"
+  },
+  {
+    "revision": "62d23dd4052d525e8de4a6f7c58865b6",
+    "url": "./static/media/error.62d23dd4.svg"
+  },
+  {
+    "revision": "4ba2843a18d4113eacf6d6b1530edbe1",
+    "url": "./static/media/exit.4ba2843a.svg"
+  },
+  {
+    "revision": "c773ff94f4a897474c7845fc81dfc6aa",
+    "url": "./static/media/loading.c773ff94.svg"
+  },
+  {
+    "revision": "a88f77554bd97c8632e77132d37d6e8e",
+    "url": "./static/media/search.a88f7755.svg"
+  },
+  {
+    "revision": "352beb52b7a049656d8854a7d9ca682e",
+    "url": "./static/media/send.352beb52.svg"
+  }
+]);
